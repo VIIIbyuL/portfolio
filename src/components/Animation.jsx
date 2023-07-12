bce40fd8eaa16ccc9cb2dd4b7f246fda11cade49
VIIIbyuL <myjjk03@gmail.com>
2023-07-12 02:45:42 -0400

    prettier ran

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -2,11 +2,15 @@
 import React from "react";
 
 function Animation() {
-    return (
-        <div className="mt-10 w-full flex justify-center align-middle">
-            <img src="src/images/ghibli.png" alt="not loading" className="ease-animation w-24 h-24 drop-shadow-lg rounded-full" />
-        </div>
-    )
+  return (
+    <div className="mt-10 w-full flex justify-center align-middle">
+      <img
+        src="src/images/ghibli.png"
+        alt="not loading"
+        className="ease-animation w-24 h-24 drop-shadow-lg rounded-full"
+      />
+    </div>
+  );
 }
 
 export default Animation;
--- src/components/SkillsBar.jsx
+++ src/components/SkillsBar.jsx
@@ -2,23 +2,25 @@
 import React from "react";
 
 function SkillsBar() {
-    return (
-        <div className="bg-[#808080] h-[250px] w-full flex justify-center">
-            <ul className="grid grid-cols-4 grid-rows-2 w-1/2">
-                <li className="flex text-center justify-center items-center">
-                    react
-                </li>
+  return (
+    <div className="bg-[#808080] h-[250px] w-full flex justify-center">
+      <ul className="grid grid-cols-4 grid-rows-2 w-1/2">
+        <li className="flex text-center justify-center items-center">react</li>
 
-                <li className="flex text-center justify-center items-center">tailwind</li>
-                <li className="flex text-center justify-center items-center">htmml</li>
-                <li className="flex text-center justify-center items-center">css</li>
-                <li className="flex text-center justify-center items-center">javascript</li>
-                <li className="flex text-center justify-center items-center">python</li>
-                <li className="flex text-center justify-center items-center">java</li>
-                <li className="flex text-center justify-center items-center">sql</li>
-            </ul>
-        </div>
-    )
+        <li className="flex text-center justify-center items-center">
+          tailwind
+        </li>
+        <li className="flex text-center justify-center items-center">htmml</li>
+        <li className="flex text-center justify-center items-center">css</li>
+        <li className="flex text-center justify-center items-center">
+          javascript
+        </li>
+        <li className="flex text-center justify-center items-center">python</li>
+        <li className="flex text-center justify-center items-center">java</li>
+        <li className="flex text-center justify-center items-center">sql</li>
+      </ul>
+    </div>
+  );
 }
 
 export default SkillsBar;
