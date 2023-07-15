88581c19183355d8ecc6086b54f7f274d3976dad
VIIIbyuL <myjjk03@gmail.com>
2023-07-15 01:50:08 -0400

    set an dependency for the ghiblitext to load before loading ghibli tier

--- src/components/GhibliText.jsx
+++ src/components/GhibliText.jsx
@@ -1,15 +1,24 @@
+
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { useState, useEffect } from "react";
 
-function GhibliText() {
+function GhibliText({ onComponentDone }) {
+    const simulateAsyncOperation = async () => {
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a 2-second delay
+        onComponentDone(); 
+    };
+
   const [showGif, setShowGif] = useState(true);
   const [showText, setShowText] = useState(false);
 
   useEffect(() => {
+    
     const timer = setTimeout(() => {
       setShowGif(false);
       setShowText(true);
+      simulateAsyncOperation();
     }, 2100); // Delay of 5000 milliseconds (5 seconds)
 
     return () => {
--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,16 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Navigation from "../components/Navigation";
-import Ghibli from "../components/Ghibli";
+import GhibliText from "../components/GhibliText";
+import GhibliTier from "../components/GhibliTier";
+import { useState } from "react";
 
 // lets make the about page about my interests
 function Aboutpage() {
+  const [componentDone, setComponentDone] = useState(false);
+
+  const handleComponentDone = () => {
+    setComponentDone(true);
+  }
+
   return (
     <div>
       <Navigation />
 
       {/* Ghibli */}
-      <Ghibli />
+      <GhibliText onComponentDone = {handleComponentDone} />
+      <GhibliTier componentDone = { componentDone }/>
     </div>
   );
 }
